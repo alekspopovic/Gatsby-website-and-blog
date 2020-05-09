299856055499eb6d386ecda9d0f6079b0ae10a73
Aleks Popovic <aleksandar.popovic@lanus.rs>
2020-05-09 19:04:55 +0200

    Tags page

---
+++ src/pages/tags.js
@@ -0,0 +1,49 @@
+import React from "react"
+import Layout from "../components/layout"
+import { Link, graphql } from "gatsby"
+import SEO from "../components/seo"
+import tagStyles from "../styles/tags.module.css"
+
+var kebabCase = require("lodash.kebabcase")
+
+const TagsPage = ({
+  data: {
+    allMarkdownRemark: { group },
+    site: {
+      siteMetadata: { title },
+    },
+  },
+  location,
+}) => (
+  <Layout location={location} title={title} headerText="Tags">
+    <SEO
+      title="all tags"
+      keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+    />
+    <div className={tagStyles.tags}>
+        {group.map(tag => (
+            <Link key={tag.fieldValue} to={`/tags/${kebabCase(tag.fieldValue)}/`}>
+                {tag.fieldValue} <span>{tag.totalCount}</span>
+            </Link>
+        ))}
+    </div>
+  </Layout>
+)
+
+export default TagsPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(limit: 2000) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`
