1277e707884ade3bccabd6f054cfa2d9592e773d
Aleks Popovic <aleksandar88popovic@gmail.com>
2023-11-03 00:53:37 +0100

    Changed Twitter summary to large image

--- src/components/seo.js
+++ src/components/seo.js
@@ -73,7 +73,7 @@ function SEO({ description, lang, meta, title, image, pagePath }) {
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary_large_image`,
         },
         {
           name: `twitter:creator`,
